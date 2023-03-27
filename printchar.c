b511c24c89e2cef2068d93321570d251aaa3b745
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-03-27 11:22:20 -0700

    new commit

---
+++ printchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_char - prints a char byte to the stdout
+ * @types: List a of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars(bytes) printed
+ */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char c = va_arg(types, int);
+
+	return (handle_write_char(c, buffer, flags, width, precision, size));
+}
