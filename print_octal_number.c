8e7984de7c0d8d02f439da4e8f2bc992acdad976
ziraabubakarbello <ziraabubakarbello@gmail.com>
2023-03-28 06:58:32 -0700

    corrected codes

--- print_octal_number.c
+++ print_octal_number.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * print_hexa - Prints a hexadecimal number in lower or upper
+ * print_octal - Prints an unsigned number in octal notation
  * @types: Lista of arguments
- * @map_to: Array of values to map the number to
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @flag_ch: Calculates active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * @size: Size specification
  * Return: Number of chars printed
  */
-int print_hexa(va_list types, char map_to[], char buffer[],
-	int flags, char flag_ch, int width, int precision, int size)
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
+
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
@@ -31,15 +29,12 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 
 	while (num > 0)
 	{
-		buffer[i--] = map_to[num % 16];
-		num /= 16;
+		buffer[i--] = (num % 8) + '0';
+		num /= 8;
 	}
 
 	if (flags & F_HASH && init_num != 0)
-	{
-		buffer[i--] = flag_ch;
 		buffer[i--] = '0';
-	}
 
 	i++;
 
--- print_unsigned_hexadecimal.c
+++ print_unsigned_hexadecimal.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- * print_percent - Prints a percent sign to the stdout
+ * print_hexadecimal - Prints an unsigned number in hexadecimal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width.
+ * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_percent(va_list types, char buffer[],
+int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	UNUSED(types);
-	UNUSED(buffer);
-	UNUSED(flags);
-	UNUSED(width);
-	UNUSED(precision);
-	UNUSED(size);
-	return (write(1, "%%", 1));
+	return (print_hexa(types, "0123456789abcdef", buffer,
+		flags, 'x', width, precision, size));
 }
