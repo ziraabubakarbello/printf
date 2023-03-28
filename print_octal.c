b2d6f79d8443b674af26449f5b4ff6d850880aad
Kehinde <Kennyakinbowale@gmail.com>
2023-03-27 20:51:40 -0700

    Partner commit

--- print_octal.c
+++ print_octal.c
@@ -1,20 +1,16 @@
-#include "main.h"
-
+#include"main.h"
 /**
-* print_hexa - Prints a hexadecimal number in lower or upper
+* print_octal - Prints an unsigned number in octal notation
 * @types: Lista of arguments
-* @map_to: Array of values to map the number to
 * @buffer: Buffer array to handle print
-* @flags:  Calculates active flags
-* @flag_ch: Calculates active flags
+* @flags: Calculates active flags
 * @width: get width
 * @precision: Precision specification
 * @size: Size specifier
-* @size: Size specification
 * Return: Number of chars printed
 */
-int print_hexa(va_list types, char map_to[], char buffer[],
-int flags, char flag_ch, int width, int precision, int size)
+int print_octal(va_list types, char buffer[],
+int flags, int width, int precision, int size)
 {
 int i = BUFF_SIZE - 2;
 unsigned long int num = va_arg(types, unsigned long int);
@@ -26,14 +22,11 @@ buffer[i--] = '0';
 buffer[BUFF_SIZE - 1] = '\0';
 while (num > 0)
 {
-buffer[i--] = map_to[num % 16];
-num /= 16;
+buffer[i--] = (num % 8) + '0';
+num /= 8;
 }
 if (flags & F_HASH && init_num != 0)
-{
-buffer[i--] = flag_ch;
 buffer[i--] = '0';
-}
 i++;
 return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
